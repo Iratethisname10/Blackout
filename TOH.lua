d1580b0379018559904af02f7f95b39f42deb68a
Iratethisname10 <115908538+Iratethisname10@users.noreply.github.com>
2022-11-02 07:51:09 +0800

    Update TOH.lua

--- TOH.lua
+++ TOH.lua
@@ -1,6 +1,6 @@
 ---@diagnostic disable: undefined-global
 
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/Iratethisname10/Script/main/RayFieldUI.lua'))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 Rayfield:Notify("Sex is good","have sex with Vo",10010348543)
 
 
@@ -89,37 +89,16 @@ local Button = MiscTab:CreateButton({
 	Callback = function()
 		local FaggotSlut = game:GetService("Workspace").tower.finishes:FindFirstChild("Finish")
 		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = FaggotSlut.CFrame
-		if game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame ~= FaggotSlut.CFrame then
-			Rayfield:Notify("BlackOut","Failed",11430595837)
-		end
 		if game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame == FaggotSlut.CFrame then
-			Rayfield:Notify("BlackOut","Success",11430595837)
+			Rayfield:Notify("BlackOut","Finnished",11430595837)
 		end
 	end,
 })
 
 local Button = MiscTab:CreateButton({
-	Name = "Partial disabler",
+	Name = "Copy Appeal Text",
 	Callback = function()
-		Rayfield:Notify("BlackOut","Attempting to disable",10010348543)
-		game:GetService("ReplicatedStorage").globalSpeed.Value = 1.4
-		wait(3)
-		game:GetService("ReplicatedStorage").globalSpeed.Value = 16
-		workspace.Gravity = 147.14999999999998
-		game:GetService("ReplicatedStorage").globalJumps.Value = 0
-		Rayfield:Notify("BlackOut","Disabled",11430595837)
-	end,
-})
-
-local Button = MiscTab:CreateButton({                                         
-	Name = "Enable AC",
-	Callback = function()
-		Rayfield:Notify("BlackOut","Attempting to disable",10010348543)
-		game:GetService("ReplicatedStorage").globalSpeed.Value = 0
-		wait(3)
-		game:GetService("ReplicatedStorage").globalSpeed.Value = 16
-		workspace.Gravity = 196.2
-		game:GetService("ReplicatedStorage").globalJumps.Value = 0
-		Rayfield:Notify("BlackOut","Disabled",11430595837)
+		setclipboard("I acknowledge that I have been banned justly for exploiting and hereby agree that I will not to do so again. Furthermore agree to have all my progress in the game deleted and wait 7 more days until I am am unbanned.")
+		Rayfield:Notify("BlackOut","Appeal text copied to clipboard",11430595837)
 	end,
 })
