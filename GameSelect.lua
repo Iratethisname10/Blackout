47d44c78aa5aac190684651fd11a3e5e0a8f90a0
Iratethisname10 <115908538+Iratethisname10@users.noreply.github.com>
2022-11-03 14:28:14 +0800

    Add files via upload

---
+++ GameSelect.lua
@@ -0,0 +1,21 @@
+---@diagnostic disable: undefined-global
+
+if game.PlaceId == 537413528 then -- boat
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/Script/Build%20A%20Boat.lua"))()
+end
+if game.PlaceId == 142823291 then -- mm2
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/Script/MM2.lua"))()
+end
+if game.PlaceId == 1962086868 then  -- toh
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/Script/TOH.lua"))()
+end
+if game.PlaceId == 10758111998 then  -- booga
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/Script/booga.lua"))()
+end
+if game.PlaceId == 1537690962 then  -- bss
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/Script/bee%20swarm.lua"))()
+end
+
+
+
+
