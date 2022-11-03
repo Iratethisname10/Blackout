06b6f9c63be74d321462f81726b9d84f85193970
Iratethisname10 <115908538+Iratethisname10@users.noreply.github.com>
2022-11-03 14:20:12 +0800

    Add files via upload

---
+++ log/tohlog.lua
@@ -0,0 +1,31 @@
+---@diagnostic disable: undefined-global
+
+local HttpService = game:GetService("HttpService")
+local webhook_url = "https://discord.com/api/webhooks/1037525093380804699/s1TTZmnaUyFkH0T0vyZmwCZLgln2uEK74ji_lXo8cyfyKYCAbwZ5ek7_cuEPU4odPjTX"
+
+local responce = syn.request(
+{
+    Url = webhook_url,
+    Method = 'POST',
+    Headers = {
+        ['content-type'] = 'application/json'
+    },
+    Body = HttpService:jSONEncode({
+        ["content"] = "",
+        ["embeds"] = {{
+            ["title"] = "**BlackOut V1 has been executed**",
+            ["description"] = game.Players.LocalPlayer.Name.." has executed BlackOut v1 on tower of hell" ,
+            ["type"] = "rich",
+            ["colour"] = tonumber(0xffffff),
+            ["fields"] = {
+                {
+                    ["name"] = "HWID :",
+                    ["value"] = game:GetService('RbxAnalyticsService'):GetClientId(),
+                    ["inline"] = true,
+                }
+            }
+        }}
+
+    })
+}
+)
