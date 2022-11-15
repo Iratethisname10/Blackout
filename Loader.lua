1551ed63c7a20095a71373cb0ef22cbab14fc6e3
Iratethisname10 <115908538+Iratethisname10@users.noreply.github.com>
2022-11-16 07:57:55 +0800

    Add files via upload

--- Loader.lua
+++ Loader.lua
@@ -12,9 +12,6 @@ end
 if game.PlaceId == 10758111998 then  -- booga
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/Script/booga.lua"))()
 end
-if game.PlaceId == 1537690962 then  -- bss
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/Script/bee%20swarm.lua"))()
-end
 if game.PlaceId == 189707 then  --nds
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/Script/NDS.lua"))()
 end
