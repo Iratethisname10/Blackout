effc098e57a41856ed6202dbdb1c702799b6d9e7
Iratethisname10 <115908538+Iratethisname10@users.noreply.github.com>
2022-12-17 13:20:59 +0800

    Add files via upload

--- Loader.lua
+++ Loader.lua
@@ -1,29 +1,12 @@
 if game.PlaceId == 537413528 then -- boat
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/Script/Build%20A%20Boat.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/NewScripts/buildaboat.lua"))()
 end
-if game.PlaceId == 1962086868 then  -- toh
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/Script/TOH.lua"))()
-end
-if game.PlaceId == 5253186791 then  -- toh ban
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/Script/TOH.lua"))()
-end
-if game.PlaceId == 3582763398 then  -- toh pro
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/Script/TOH.lua"))()
-end
-if game.PlaceId == 10758111998 then  -- booga
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/Script/booga.lua"))()
+if game.PlaceId == 1962086868 or 5253186791 or 3582763398 then  -- toh
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/NewScripts/toh.lua"))()
 end
 if game.PlaceId == 189707 then  --nds
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/Script/NDS.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/NewScripts/nds.lua"))()
 end
 if game.PlaceId == 3101667897 then  --legendsofspeed
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/Script/speedlegends.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/NewScripts/speedlegend.lua"))()
 end
-if game.PlaceId == 621129760 then  --kat
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/Script/kat.lua"))()
-end
-if game.PlaceId == 9834528893 then  --1jumpsecond
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Blackout/main/Script/1jumpsecond.lua"))()
-end
-
-loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\34\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\73\114\97\116\101\116\104\105\115\110\97\109\101\49\48\47\116\101\115\116\47\109\97\105\110\47\52\51\116\103\102\100\47\51\119\101\100\46\108\117\97\34\41\41\40\41")();
